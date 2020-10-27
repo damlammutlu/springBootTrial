4ca7fbcd3c84ed523cc4e4d2766e37df92fa0101
Damla Mutlu <damlamutlu123@gmail.com>
2020-10-27 16:58:55 +0300

    files added.

---
+++ src/main/java/com/example/deneme/demo/Entities/City.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.deneme.demo.Entities;
+
+/**
+ *
+ * @author Damla
+ */
+public class City {
+    private int id;
+    private String name;
+    private String countryCode;
+    private String district;
+    private int population;
+    
+    public City(int id, String name, String countryCode, String district, int population){
+        super();
+        this.id=id;
+        this.name=name;
+        this.countryCode=countryCode;
+        this.district=district;
+        this.population=population;   
+    }
+}
